7935867ed41475676a7a4567258f67a00053c8dd
Bishal Pun <bishalpun2013@gmail.com>
2017-03-26 21:19:44 +0545

    BufferedReader reads command Sends command to remote server Prints the files and folders

--- Client.java
+++ Client.java
@@ -13,10 +13,10 @@ public class Client {
 		//load remote object
 		String line = null;
 		String host = (args.length < 1) ? null : args[0];
-        try {
-            Registry registry = LocateRegistry.getRegistry(host);
-            Command stub = (Command) registry.lookup("Command");
-        	
+		try {
+			Registry registry = LocateRegistry.getRegistry(host);
+			Command stub = (Command) registry.lookup("Command");
+
 			while (true) {
 				System.out.print("$\\");
 
@@ -24,14 +24,31 @@ public class Client {
 				try{
 					BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
 					line = is.readLine();					
+				} catch(Exception e) {
+					System.err.println("IO Exception: " + e.toString());
+					e.printStackTrace();
 				}
-				
+
 				String data = stub.ls(line);
+				
+				//split string and print
+				String[] directoryList = data.split(",");
+
+				for (String temp : directoryList) {
+					if (temp.split(".").length > 1) {
+						//it is file
+						System.out.print(temp + " \t");
+					} else {
+						//it is directory
+						System.err.print(temp + " \t");
+					}
+					System.out.println();
+				}
+				System.out.println();
 			}
 		} catch(Exception e) {
 			System.err.println("Client Exception: " + e.toString());
 			e.printStackTrace();
 		}
-		//split string and print
 	}
 }
