98f9db14bf13d01d4edf79d9d7ba750f37ebf3ac
Bishal Pun <bishalpun2013@gmail.com>
2017-03-26 21:23:04 +0545

    Provides interface which is implemented by server

--- Command.java
+++ Command.java
@@ -2,5 +2,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Command extends Remote{
-	String ls() throws RemoteException;
+	String ls(String s) throws RemoteException;
 }
